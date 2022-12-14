bcc853bc540f378ded8a35cfd594531bc987fd74
Lokesh Ghosh <lghosh857@gmail.com>
2022-12-14 14:00:03 +0530

    .

---
+++ src/Basic/First.java
@@ -0,0 +1,10 @@
+package Basic;
+
+public class First {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+
+	}
+
+}
---
+++ src/Simple_Interest.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Simple_Interest {
+
+	public static void main(String[] args) {
+		 Scanner sc= new Scanner(System.in);
+	        System.out.println("Enter the priciple amount :");
+	        float p= sc.nextFloat();
+	        System.out.println("Enter the rate :");
+	        float r= sc.nextFloat();
+	        System.out.println("Enter the time :");
+	        float t= sc.nextFloat();
+	        float si= (p*t*r)/100;
+	        System.out.println("The simple interest :"+si);
+	        System.out.println("The total amount is :"+(si+p));
+	}
+
+}
