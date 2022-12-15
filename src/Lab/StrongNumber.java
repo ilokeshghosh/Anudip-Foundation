0571cfe190f7c07e372094ce6b229889cb3072ed
Lokesh Ghosh <lghosh857@gmail.com>
2022-12-15 20:57:58 +0530

    Update StrongNumber.java

--- src/Lab/StrongNumber.java
+++ src/Lab/StrongNumber.java
@@ -1,11 +1,44 @@
 package Lab;
+
+import java.util.Scanner;
+
 //WAP to check whether a number is  strong number or not
 
 public class StrongNumber {
 
 	public static void main(String[] args) {
-		
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter any number to check whether");
+		int num = sc.nextInt();
+		int tmp=num;
+		int sum=0;
+		while(num>0) {
+			int r = num%10;
+			sum+=fact(r);
+			num/=10;
+		}
+		if(sum==tmp) {
+			System.out.println(tmp+" is strong number ");
+		}
+		else {
+			System.out.println(tmp+" is not a strong number ");
+		}
+		sc.close();
 
+	}
+	static int fact(int num) {
+		int mul=1;
+		for(int i=1;i<=num;i++) {
+			if(num==0) {
+				return 0;
+			}
+			if(num==1) {
+				mul*=1;
+			}
+			
+			mul*=i;
+		}
+		return mul;
 	}
 
 }
