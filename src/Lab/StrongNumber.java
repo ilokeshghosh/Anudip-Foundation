74a82b353eb74610492d20fcf01f2750f00af62b
Lokesh Ghosh <lghosh857@gmail.com>
2022-12-15 21:08:10 +0530

    Update StrongNumber.java

--- src/Lab/StrongNumber.java
+++ src/Lab/StrongNumber.java
@@ -7,16 +7,30 @@ import java.util.Scanner;
 public class StrongNumber {
 
 	public static void main(String[] args) {
+		
+		//Taking input
 		Scanner sc= new Scanner(System.in);
 		System.out.println("Enter any number to check whether");
 		int num = sc.nextInt();
+		
+		//Storing num in temp variable to compare later
 		int tmp=num;
 		int sum=0;
+		
+		//running loop to find factorial and add them  
 		while(num>0) {
+			
+			//taking out last digit of a number by taking out remainder;
 			int r = num%10;
+			
+			//calling a function that returns the factorial of the remainder
 			sum+=fact(r);
+			
+			//Resizing the number
 			num/=10;
 		}
+		
+		//Checking whether the number is strong or not
 		if(sum==tmp) {
 			System.out.println(tmp+" is strong number ");
 		}
@@ -26,6 +40,8 @@ public class StrongNumber {
 		sc.close();
 
 	}
+	
+	// function that return the factorial of remainder 
 	static int fact(int num) {
 		int mul=1;
 		for(int i=1;i<=num;i++) {
