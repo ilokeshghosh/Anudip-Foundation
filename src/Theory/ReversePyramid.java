07735ddcfaa9360d25944b676e8101baf62f2f0d
Lokesh Ghosh <lghosh857@gmail.com>
2022-12-16 14:03:36 +0530

    Update ReversePyramid.java

--- src/Theory/ReversePyramid.java
+++ src/Theory/ReversePyramid.java
@@ -1,10 +1,15 @@
 package Theory;
 
+import java.util.Scanner;
+
 public class ReversePyramid {
 
 	public static void main(String[] args) {
-		for(int i=1;i<=5;i++) {
-			for(int j=5;j>i;j--) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a number :");
+		int n= sc.nextInt();
+		for(int row=1;row<=n;row++) { // row
+			for(int col=n;col>=row;col--) { //col
 				System.out.print("* ");
 			}
 			System.out.println();
