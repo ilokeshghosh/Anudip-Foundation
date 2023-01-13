6673baf10208121065fbb00f20b1d36dc27b5829
Lokesh Ghosh <lghosh857@gmail.com>
2023-01-13 13:15:34 +0530

    .

---
+++ src/CollectionFramework/Student.java
@@ -0,0 +1,25 @@
+package CollectionFramework;
+
+public class Student {
+	int id;
+	String name;
+	int roll;
+	
+	public int getId() {
+		return id;
+	}
+	public String getName() {
+		return name;
+	}
+	
+	public int getRoll() {
+		return roll;
+	}
+	
+	public Student(int id, String name, int roll) {
+		super();
+		this.id=id;
+		this.name=name;
+		this.roll=roll;
+	}
+}
