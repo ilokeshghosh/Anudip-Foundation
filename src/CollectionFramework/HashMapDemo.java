40ffbaeb6f25e282cee1e6eefa2285440dea958b
Lokesh Ghosh <lghosh857@gmail.com>
2023-01-17 13:15:20 +0530

    .

---
+++ src/CollectionFramework/HashMapDemo.java
@@ -0,0 +1,15 @@
+package CollectionFramework;
+
+import java.util.*;
+
+public class HashMapDemo {
+
+	public static void main(String[] args) {
+		HashMap<Integer, String> hmap = new HashMap<Integer, String>();
+		hmap.put(101, "Amrita");
+		hmap.put(107, "Rahul");
+		hmap.put(107, "Shawin");
+			
+	}
+
+}
